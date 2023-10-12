144bccf1c92a8974dcdc78ecad4ac3a9c2aeb437
AlphaMagimel <alban.magi@gmail.com>
2023-10-12 16:31:02 +0200

    comme

---
+++ theme1/Question1/home.js
@@ -0,0 +1,38 @@
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let loi = document.getElementById("loi");
+let next = document.getElementById("next");
+
+btn1.onclick = function() {
+    next.style.display = "block";
+    btn1.className = "btn btn-success btn-block";
+    btn2.className = "btn btn-danger btn-block";
+    btn3.className = "btn btn-danger btn-block";
+    btn4.className = "btn btn-danger btn-block";
+}
+
+btn2.onclick = function() {
+    next.style.display = "block";
+    btn1.className = "btn btn-success btn-block";
+    btn2.className = "btn btn-danger btn-block";
+    btn3.className = "btn btn-danger btn-block";
+    btn4.className = "btn btn-danger btn-block";
+}
+
+btn3.onclick = function() {
+    next.style.display = "block";
+    btn1.className = "btn btn-success btn-block";
+    btn2.className = "btn btn-danger btn-block";
+    btn3.className = "btn btn-danger btn-block";
+    btn4.className = "btn btn-danger btn-block";
+}
+
+btn4.onclick = function() {
+    next.style.display = "block";
+    btn1.className = "btn btn-success btn-block";
+    btn2.className = "btn btn-danger btn-block";
+    btn3.className = "btn btn-danger btn-block";
+    btn4.className = "btn btn-danger btn-block";
+}
